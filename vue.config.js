65dd38454469922730cc6a9c09784e91d9b4726f
egorkdrummer <egorkdrummer@gmail.com>
2019-04-03 13:15:22 +0300

    upd

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,8 @@ const path = require("path");
 const PrerenderSPAPlugin = require("prerender-spa-plugin");
 //const r = require(__dirname + "/routes");
 module.exports = {
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/vue-test-prerender/" : "/",
   configureWebpack: {
     plugins: [
       new PrerenderSPAPlugin({
